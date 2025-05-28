ab1160816f482c022f0d5004cae366a9d9fdbf01
ErEr <jeffliao92316046@gmail.com>
2025-05-29 01:13:43 +0800

    feat: train_model and refactor other code

--- src/collect_data.py
+++ src/collect_data.py
@@ -10,7 +10,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 
 def find_next_img_number():
-    output_dir = Path("./dataset")
+    output_dir = Path("./dataset/origin")
     txt_files = [f for f in output_dir.iterdir() if f.is_file()]
     return len(txt_files)
 
@@ -19,9 +19,9 @@ def collect_data():
     driver.get(URL)
     driver.implicitly_wait(5)
     img = driver.find_element(By.XPATH,'//*[@id="Panel_bshtm"]/table/tbody/tr/td/table/tbody/tr[1]/td/div/div[1]/img')
-    img = img.screenshot(f"dataset/{find_next_img_number()+1}.png")
+    img = img.screenshot(f"dataset/origin/{find_next_img_number()+1}.png")
     driver.close()
 
-for i in range(100):
+for i in range(500):
     collect_data()
     time.sleep(random.randint(3,10))
